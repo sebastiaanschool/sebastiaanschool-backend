88900304c2c7ce3e3b8a7391227cbf808b472efe
Barend Garvelink <bgarvelink@xebia.com>
2016-03-09 23:41:19 +0100

    Allow posting news and bulletins ahead of time.
    Filter future newsletters/bulletins by default. Append /all/ to the URL to
    include them.

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from datetime import date
+
+from rest_framework.decorators import list_route
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+
 from backend.models import AgendaItem, Bulletin, ContactItem, NewsLetter
 from rest_framework import viewsets
 from backend.serializers import AgendaItemSerializer, BulletinSerializer, ContactItemSerializer, NewsLetterSerializer
@@ -15,9 +21,19 @@ class AgendaItemViewSet(viewsets.ModelViewSet):
 class BulletinViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows bulletins to be viewed or edited.
+
+    Shows only bulletins where the publishedAt date is not in the future. Append `/all/` to the request path to include
+    future bulletins (admins only).
     """
-    queryset = Bulletin.objects.all().order_by('-publishedAt')
+    queryset = Bulletin.objects.exclude(publishedAt__gt=date.today()).order_by('-publishedAt')
     serializer_class = BulletinSerializer
+
+    @list_route(permission_classes=[IsAdminUser])
+    def all(self, request):
+        selection = Bulletin.objects.all().order_by('-publishedAt')
+        serializer = self.get_serializer(selection, many=True)
+        return Response(serializer.data)
+
 
 class ContactItemViewSet(viewsets.ModelViewSet):
     """
@@ -29,6 +45,15 @@ class ContactItemViewSet(viewsets.ModelViewSet):
 class NewsLetterViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows news letters to be viewed or edited.
+
+    Shows only bulletins where the publishedAt date is not in the future. Append `/all/` to the request path to include
+    future newsletters (admins only).
     """
-    queryset = NewsLetter.objects.all()
+    queryset = NewsLetter.objects.exclude(publishedAt__gt=date.today()).order_by('-publishedAt')
     serializer_class = NewsLetterSerializer
+
+    @list_route(permission_classes=[IsAdminUser])
+    def all(self, request):
+        selection = NewsLetter.objects.all().order_by('-publishedAt')
+        serializer = self.get_serializer(selection, many=True)
+        return Response(serializer.data)

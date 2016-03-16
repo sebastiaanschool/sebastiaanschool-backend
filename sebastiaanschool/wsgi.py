f674c2addfd72bee61c8a5bf1ea115c5a73c1e2c
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2016-03-16 19:47:33 +0100

    Make loading openshift virtual env conditional.

--- sebastiaanschool/wsgi.py
+++ sebastiaanschool/wsgi.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+if 'OPENSHIFT_PYTHON_DIR' in os.environ:
+    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+    try:
+        execfile(virtualenv, dict(__file__=virtualenv))
+    except IOError:
+        pass
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path

4cbb3fbbcbed36aa2b2d600824082e3f42897934
Barend Garvelink <bgarvelink@xebia.com>
2016-03-25 23:01:18 +0100

    Implement __unicode__ voodoo on model.
    https://docs.djangoproject.com/en/dev/topics/python3/#str-and-unicode-methods

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 
 
+@python_2_unicode_compatible
 class AgendaItem(models.Model):
     title = models.CharField(max_length=140)
     type = models.CharField(max_length=140)
@@ -16,6 +18,7 @@ class AgendaItem(models.Model):
         ordering = ['-start']
 
 
+@python_2_unicode_compatible
 class Bulletin(models.Model):
     title = models.CharField(max_length=140)
     body = models.TextField()
@@ -28,6 +31,7 @@ class Bulletin(models.Model):
         ordering = ['-publishedAt']
 
 
+@python_2_unicode_compatible
 class ContactItem(models.Model):
     displayName = models.CharField(max_length=140)
     email = models.CharField(max_length=500)
@@ -41,6 +45,7 @@ class ContactItem(models.Model):
         ordering = ['order']
 
 
+@python_2_unicode_compatible
 class NewsLetter(models.Model):
     title = models.CharField(max_length=140)
     documentUrl = models.CharField(max_length=500)

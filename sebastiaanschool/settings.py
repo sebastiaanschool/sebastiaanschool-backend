8de4a275bd1ca0c1f4bb2bbc4419ad7868c0a23b
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2016-10-09 22:43:19 +0200

    Initial work for push notifications.

--- sebastiaanschool/settings.py
+++ sebastiaanschool/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'push_notifications',
     'backend',
 ]
 
@@ -149,6 +150,15 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+
+DATA_DIR = os.getenv('OPENSHIFT_DATA_DIR', BASE_DIR)
+
+PUSH_NOTIFICATIONS_SETTINGS = {
+        "GCM_API_KEY": "[your api key]",
+        "APNS_CERTIFICATE": os.path.join(DATA_DIR, 'SebastiaanPushCertificaten.p12'),
+        "WNS_PACKAGE_SECURITY_ID": "[your package security id, e.g: 'ms-app://e-3-4-6234...']",
+        "WNS_SECRET_KEY": "[your app secret key, e.g.: 'KDiejnLKDUWodsjmewuSZkk']",
+}
 
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
